9b5019887fa1189055dc0c0593e550c682befb3b
Madanbhanani <bhananimadan@gmail.com>
2024-06-23 01:57:47 -0700

    blub on off

---
+++ index.js
@@ -0,0 +1,6 @@
+function btnOn() {
+    document.getElementById("div").style.backgroundImage = "url('pic_bulbon.gif')";
+}
+function btnOff() {
+    document.getElementById("div").style.backgroundImage = "url('pic_bulboff.gif')";
+}
